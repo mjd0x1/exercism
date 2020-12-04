f2c8ce9a57bba00f02e96bab0635193be88b263d
Michael Deer <mikejdeer@gmail.com>
2020-12-04 20:36:32 +0000

    clojure go

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,17 @@
 (ns bob)
+(require '[clojure.string :as str])
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
+(defn response-for [s] 
+  (let [sn (str/trim s)
+        isQuestion (= (-> sn seq last) \?)
+        allUpper (= (drop-last sn) (drop-last (str/upper-case sn)))
+        atLeastOneLetter (some true? (map #(Character/isLetter %) sn))]
+               
+       (cond
+         (= sn "") "Fine. Be that way!"
+         (and isQuestion allUpper atLeastOneLetter) "Calm down, I know what I'm doing!"
+         isQuestion "Sure."
+         (and allUpper atLeastOneLetter) "Whoa, chill out!"
+         :else "Whatever."))
+  
 )
--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -7,24 +7,15 @@ import (
 //Distance ...Calculate Hamming Distance
 func Distance(a, b string) (int, error) {
 
-	checkCell := func(a, b byte) int {
-		value := 0
-		if a != b {
-			value = 1
-		}
-		return value
-	}
-
 	if len(a) != len(b) {
-		return 0, errors.New("Must be the same length")
-	}
-	if (len(a) == len(b)) && a == "" {
-		return 0, nil
+		return 0, errors.New("must be the same length")
 	}
 
 	sum := 0
 	for i := 0; i < len(a); i++ {
-		sum += checkCell(a[i], b[i])
+		if a[i] != b[i] {
+			sum++
+		}
 	}
 	return sum, nil
 }
