a9c55d555315f0df9e92b7302e8e1ae324f0a3c1
Michael Deer <mikejdeer@gmail.com>
2020-12-10 19:38:10 +0000

    clojure/javascript changes

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,17 +1,24 @@
 (ns bob)
 (require '[clojure.string :as str])
 
+(defn- question? [s] 
+  (str/ends-with? s "?"))
+(defn- all-upper? [s] 
+  (= s (str/upper-case s)))
+(defn- includes-one-letter? [s] 
+  (seq (filter #(Character/isLetter %) s)))
+(def silence? str/blank?)
+(defn- shouting? [s]
+  (and (all-upper? s) (includes-one-letter? s)))
+(defn- forceful-question? [s] 
+  (and (question? s) (shouting? s)))
+
+
 (defn response-for [s] 
-  (let [sn (str/trim s)
-        isQuestion (= (-> sn seq last) \?)
-        allUpper (= (drop-last sn) (drop-last (str/upper-case sn)))
-        atLeastOneLetter (some true? (map #(Character/isLetter %) sn))]
-               
+  (let [sn (str/trim s)]
        (cond
-         (= sn "") "Fine. Be that way!"
-         (and isQuestion allUpper atLeastOneLetter) "Calm down, I know what I'm doing!"
-         isQuestion "Sure."
-         (and allUpper atLeastOneLetter) "Whoa, chill out!"
-         :else "Whatever."))
-  
-)
+         (silence? sn) "Fine. Be that way!"
+         (forceful-question? s) "Calm down, I know what I'm doing!"
+         (question? sn) "Sure."
+         (shouting? sn) "Whoa, chill out!"
+         :else "Whatever.")))
--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,6 +1,5 @@
 "use strict";
 export const decodedValue = (resistors) => {
-  const colors = {"black": 0, "brown": 1 , "red": 2, "orange":3,"yellow": 4, "green":5, "blue":6 , "violet":7, "grey":8, "white":9}
-  const [first,second] = resistors.map(x => colors[x].toString())
-  return parseInt(first + second)
+  const colors = {"black": "0", "brown": "1" , "red": "2", "orange":"3","yellow": "4", "green":"5", "blue":"6" , "violet":"7", "grey":"8", "white":"9"}
+  return Number(colors[resistors[0]]+colors[resistors[1]])
 };
